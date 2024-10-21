73370955dd3790c5faec0b71514794b0c653e0ff
Wojtaz <279730@student.pwr.edu.pl>
2024-10-22 01:15:03 +0200

    Generowanie zmiennych p, q, n, fi_n potrzebnych w RSA. Zmienienie generatora liczb pseudolosowych na lepszy pod względem kryptograficznym

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-
-
+mod RSA;
 fn main() 
 {
-    println!("Hello, world!, :)");
+    let (p, q, n, fi_n) = RSA::generate_rsa_values();
+    println!("p: {}, q: {}, n: {}, fi_n: {}", p, q, n, fi_n);
 }
