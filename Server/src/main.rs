260a030aa9dc9fc4a867835e18b2c320f8054b85
wojteq18 <279730@student.pwr.edu.pl>
2025-02-09 00:15:59 +0100

    start pisania serwera

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::{str, thread};
+use std::process::{Command, Stdio};
+use std::io;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn handle_client(
+    stream: TcpStream, 
+    clients: Arc<Mutex<Vec<TcpStream>>>, 
+    current_client: Arc<Mutex<usize>>
+) {
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
