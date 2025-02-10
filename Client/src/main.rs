74fc8eeb43f0e03ea5409b25a6b14bb0b9dda0ea
wojteq18 <279730@student.pwr.edu.pl>
2025-02-11 00:11:03 +0100

    Rozwiniecie klienta

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -2,6 +2,8 @@ use std::io::{BufReader, BufRead, Write};
 use std::net::TcpStream;
 use std::thread;
 use cryptography::unencryption;
+use cryptography::encryption::{self, encrypt};
+use cryptography::RSA;
 
 fn string_to_vec_u128(s: &str) -> Vec<u128> {
     s.chars().map(|c| c as u128).collect()
@@ -19,7 +21,7 @@ fn main() -> std::io::Result<()> {
             match line {
                 Ok(msg) => {
                     let correct_form = string_to_vec_u128(&msg);
-                    let decrypted = unencryption::decrypt(correct_form);
+                    //let decrypted = unencryption::decrypt(correct_form);
                     println!("Serwer: {}", msg) },
                 Err(e) => {
                     println!("Błąd odczytu od serwera: {}", e);
@@ -33,8 +35,12 @@ fn main() -> std::io::Result<()> {
     let stdin = std::io::stdin();
     for line in stdin.lock().lines() {
         let line = line?; // Operator propagacji błędów
-        stream.write_all(line.as_bytes())?;
-
+        //generujemy odpowiednie wartości zgodnie z algorytmem RSA
+        let (p, q, n, fi_n) = RSA::generate_rsa_values();
+        let e = RSA::generate_e(fi_n);
+        let encrypted_message = cryptography::encryption::encrypt(&line, e, n);
+        stream.write_all(&n.to_le_bytes())?;
+        //stream.write_all(&encrypted_message)?; TODO
         stream.write_all(b"\n")?; // Znak nowej linii wymagany przez serwer
     }
     Ok(())
