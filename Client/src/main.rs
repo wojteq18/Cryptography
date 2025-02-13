ffe25b136ba50ffada00d8f449fe2f2beab4b88d
wojteq18 <279730@student.pwr.edu.pl>
2025-02-13 23:52:36 +0100

    updating client

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{BufReader, BufRead, Write};
 use std::net::TcpStream;
 use std::thread;
+use std::sync::{Arc, Mutex};
 use cryptography::unencryption;
 use cryptography::encryption::{self, encrypt};
 use cryptography::RSA;
@@ -8,10 +9,16 @@ use cryptography::RSA;
 fn string_to_vec_u128(s: &str) -> Vec<u128> {
     s.chars().map(|c| c as u128).collect()
 }
+
 fn main() -> std::io::Result<()> {
+    let (p, q, n, fi_n) = RSA::generate_rsa_values();   
+    let e = RSA::generate_e(fi_n);
     // Łączenie z serwerem
     let mut stream = TcpStream::connect("127.0.0.1:9999")?;
     println!("Połączono z serwerem");
+
+    let n_shared = Arc::new(Mutex::new(0_u128));
+    let e_shared = Arc::new(Mutex::new(0_u128));
 
     // Odbieranie wiadomości od serwera
     let mut stream_clone = stream.try_clone()?; // Tworzenie klonu strumienia, by odbierać wiadomości od serwera
@@ -20,7 +27,11 @@ fn main() -> std::io::Result<()> {
         for line in reader.lines() {
             match line {
                 Ok(msg) => {
-                    let correct_form = string_to_vec_u128(&msg);
+                    if let Some(first_word) = msg.split_whitespace().next() {
+                        if let Ok(new_n) = first_word.parse::<u128>() {
+                            *n_shared.lock().unwrap() = new_n;
+                        }
+                    }
                     //let decrypted = unencryption::decrypt(correct_form);
                     println!("Serwer: {}", msg) },
                 Err(e) => {
@@ -35,13 +46,12 @@ fn main() -> std::io::Result<()> {
     let stdin = std::io::stdin();
     for line in stdin.lock().lines() {
         let line = line?; // Operator propagacji błędów
-        //generujemy odpowiednie wartości zgodnie z algorytmem RSA
-        let (p, q, n, fi_n) = RSA::generate_rsa_values();
-        let e = RSA::generate_e(fi_n);
         let encrypted_message = cryptography::encryption::encrypt(&line, e, n);
-        stream.write_all(&n.to_le_bytes())?;
-        //stream.write_all(&encrypted_message)?; TODO
-        stream.write_all(b"\n")?; // Znak nowej linii wymagany przez serwer
+        writeln!(stream, "{}", n)?;
+        writeln!(stream, "{}", e)?;
+        for i in encrypted_message.iter() {
+            writeln!(stream, "{}", i)?;
+        }
     }
     Ok(())
 }
