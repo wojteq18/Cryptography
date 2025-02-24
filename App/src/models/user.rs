0addc209b81c1ee22a95c98d8cc45ba28cf77fcf
wojteq18 <279730@student.pwr.edu.pl>
2025-02-24 20:18:02 +0100

    start writing user

--- App/src/models/user.rs
+++ App/src/models/user.rs
@@ -15,5 +15,37 @@ pub struct User {
 }
 
 impl User {
+    //sprawdź czy użytkownik istnieje
+    pub fn user_exist(conn: &mut PooledConn, login: &str) -> bool {
+        let exists: Option<String> = conn.exec_first(
+            "SELECT login FROM users WHERE login = :login",
+            params! {
+                "login" => login,
+            }
+        ).unwrap_or(None);
+        exists.is_some() //zwróc true, jeśli użytkownik istnieje
+    }
 
+    //konstruktor nowego użytkownika
+    pub fn new(login: &str, password: &str, p: &i64, q: &i64, n: &i64, fi_n: &i64) -> Self {
+        let hashed_password = match hash(password, DEFAULT_COST) {
+            Ok(h) => h,
+            Err(_) => panic!("Failed to hash password"),
+        };
+
+        User {
+            user_id: 0,
+            login: login.to_owned(),
+            password: hashed_password,
+            p: *p,
+            q: *q,
+            n: *n,
+            fi_n: *fi_n,
+        }
+    }
+
+    //dodanie użytkownika jeśli nie istnieje
+    pub fn add_user(&self, conn: &mut PooledConn) -> Result<(), mysql::Error> {
+        //TODO
+    }
 }
